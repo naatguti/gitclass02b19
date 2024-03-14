a93c4381b7ef36e15f59237cff2ca14d1a94b2ab
nataliagutierrez <naty.ag221@gmail.com>
2024-03-13 20:33:43 -0400

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,8 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");}}
+        System.out.printf("Hello World!");
+        System.out.println("Hello Mars");
+
+    }
+}
